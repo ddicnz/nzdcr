9b345f8f7732fa56aa040d848bb7d6d1efcf515c
ziyu562 <liuziyu7899@icloud.com>
2026-04-10 15:39:45 +1200

    up

--- src/data/contactForm.js
+++ src/data/contactForm.js
@@ -1,2 +1,2 @@
 /** FormSubmit forwards submissions to this inbox (first use requires confirming via FormSubmit’s email). */
-export const CONTACT_FORM_SUBMIT_URL = 'https://formsubmit.co/liuziyu7899@icloud.com'
+export const CONTACT_FORM_SUBMIT_URL = 'https://formsubmit.co/booking@nzdcr.co.nz'
